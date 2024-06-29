5dc3d09d85977014d319387a5b9b68cf1f366bd6
AngelIpina12 <angelipina12@hotmail.com>
2024-06-29 13:00:21 -0600

    cambios curso react clase 112

--- 08-proyecto2/src/components/Home.js
+++ 08-proyecto2/src/components/Home.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>
+        Hola, soy José Ángel Ipiña, soy Desarrollador Web en Nuevo León. 
+        Soy Ing. en Tecnologías Computacionales actualmente con puesto de 
+        Desarrollador JR en la empresa XCF S.A. de C.V.  
+      </h1>
+      <h2>
+        Ofrezco mis servicios para crear un sitio o aplicacion web, tener más 
+        visibilidad y relevancia en internet. Ponte en <Link to="/contacto">contacto conmigo</Link> si te interesa.
+      </h2>
+      <section className='latest-works'>
+          <h2>Proyectos</h2>
+          <p>Estos son algunos de mis proyectos como Desarrollador Web</p>
+          <div className='works'>
+
+          </div>
+      </section>
+    </div>
   )
 }
