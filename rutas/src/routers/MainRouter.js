b176f62cf9d763edf8061e80d52490f37ada83b0
AngelIpina12 <angelipina12@hotmail.com>
2024-10-17 07:11:13 -0600

    Implementación de Navbar con Navlinks

--- rutas/src/routers/MainRouter.js
+++ rutas/src/routers/MainRouter.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Routes, Link, BrowserRouter } from "react-router-dom"
+import { Route, Routes, Link, BrowserRouter, NavLink } from "react-router-dom"
 import { Home } from "../components/Home"
 import { Articles } from "../components/Articles"
 import { Contact } from "../components/Contact"
@@ -7,7 +7,21 @@ import { Error } from "../components/Error"
 
 export const MainRouter = () => {
     return (
+
         <BrowserRouter>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/home">Inicio</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/articles">Artículos</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact">Contacto</NavLink>
+                    </li>
+                </ul>
+            </nav>
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
                 <Route path="/home" element={<Home/>}></Route>
