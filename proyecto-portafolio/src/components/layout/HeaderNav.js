022a6b61b3a599c857517954d0f6693b438a5999
AngelIpina12 <angelipina12@hotmail.com>
2024-10-18 07:10:10 -0600

    Creación de headernav

--- proyecto-portafolio/src/components/layout/HeaderNav.js
+++ proyecto-portafolio/src/components/layout/HeaderNav.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 export const HeaderNav = () => {
   return (
     <header>
-        <h1>Ángel Ipiña</h1>
+        <div className='logo'>
+            <span>AI</span>
+            <h3>Ángel Ipiña DEV</h3>
+        </div>
         <nav>
             <ul>
-                <li></li>
+                <li>
+                    <NavLink to="/home">Inicio</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/portfolio">Portafolio</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/services">Servicios</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/curriculum">Curriculum</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact">Contacto</NavLink>
+                </li>
             </ul>
         </nav>
     </header>
