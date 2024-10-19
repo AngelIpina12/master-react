f116456092b5601543bbd09f68f0804ea6c1ad59
AngelIpina12 <angelipina12@hotmail.com>
2024-10-19 16:34:11 -0600

    Estilización del inicio

--- proyecto-portafolio/src/components/Home.js
+++ proyecto-portafolio/src/components/Home.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h1>
+        Hola, soy <strong>José Ángel Ipiña</strong>, desarrollador web en Nuevo León. 
+        Soy Ing. en Tecnologías Computacionales actualmente con puesto de 
+        <strong>Desarrollador JR</strong> en la empresa <strong>XCF S.A. de C.V</strong>.  
+      </h1>
+      <h2>
+        Ofrezco mis servicios para crear un sitio o aplicacion web, tener más 
+        visibilidad y relevancia en internet. Ponte en <Link to="/contact">contacto conmigo</Link> si te interesa.
+      </h2>
+      <section className='latest-works'>
+        <h2 className='heading'>Proyectos</h2>
+        <p>Estos son algunos de mis proyectos como Desarrollador Web</p>
+      </section>
+    </div>
   )
 }
--- proyecto-portafolio/src/components/Services.js
+++ proyecto-portafolio/src/components/Services.js
@@ -2,6 +2,22 @@ import React from 'react'
 
 export const Services = () => {
   return (
-    <div>Services</div>
+    <div className='page'>
+      <h1 className='heading'>Servicios</h1>
+      <section className='services'>
+        <article className='service'>
+          <h2>Aplicaciones Web</h2>
+          <p>Trabajo con ASP.NET Core 8 para creacion de aplicaciones web tipo ERP</p>
+        </article>
+        <article className='service'>
+          <h2>Diseño web</h2>
+          <p>Estilizo la página web que desees</p>
+        </article>
+        <article className='service'>
+          <h2>Posicionamiento web</h2>
+          <p>Mejoro la posición de tu página ante Google para que llegue a más personas</p>
+        </article>
+      </section>
+    </div>
   )
 }
