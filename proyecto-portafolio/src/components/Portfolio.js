5da30cc26a5bac5f3b57adb329e4c210976b4925
AngelIpina12 <angelipina12@hotmail.com>
2024-10-19 18:02:51 -0600

    Estilizacion de imagenes de portafolio, junto con sus descripciones

--- proyecto-portafolio/src/components/Portfolio.js
+++ proyecto-portafolio/src/components/Portfolio.js
@@ -6,15 +6,20 @@ export const Portfolio = () => {
   return (
     <div className='page'>
       <h1 className='heading'>Portafolio</h1>
+      <section className='works'>
       {
         works.map((work) => (
-          <article key={work.id}>
+          <article key={work.id} className='work-item'>
+            <div className='mask'>
+              <img src={"/images/" + work.id + ".png"}/>
+            </div>
             <span>{work.categories}</span>
             <h2><Link to={"/project/" + work.id}>{work.name}</Link></h2>
             <h3>{work.technologies}</h3>
           </article>
         ))
       }
+      </section>
     </div>
   )
 }
