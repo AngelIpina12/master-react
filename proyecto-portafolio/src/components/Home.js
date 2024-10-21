8518f032f1743f7aecf541416079177509b0a007
AngelIpina12 <angelipina12@hotmail.com>
2024-10-20 21:24:12 -0600

    Término de Portafolio

--- proyecto-portafolio/src/components/Home.js
+++ proyecto-portafolio/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Worklist } from './Worklist'
 
 export const Home = () => {
   return (
@@ -9,13 +10,14 @@ export const Home = () => {
         Soy Ing. en Tecnologías Computacionales actualmente con puesto de 
         <strong>Desarrollador JR</strong> en la empresa <strong>XCF S.A. de C.V</strong>.  
       </h1>
-      <h2>
+      <h2 className='title'>
         Ofrezco mis servicios para crear un sitio o aplicacion web, tener más 
         visibilidad y relevancia en internet. Ponte en <Link to="/contact">contacto conmigo</Link> si te interesa.
       </h2>
       <section className='latest-works'>
         <h2 className='heading'>Proyectos</h2>
         <p>Estos son algunos de mis proyectos como Desarrollador Web</p>
+        <Worklist limit="2"/>
       </section>
     </div>
   )
--- proyecto-portafolio/src/components/Portfolio.js
+++ proyecto-portafolio/src/components/Portfolio.js
@@ -1,25 +1,11 @@
 import React from 'react'
-import { works } from '../data/works'
-import { Link } from 'react-router-dom'
+import { Worklist } from './Worklist'
 
 export const Portfolio = () => {
   return (
     <div className='page'>
       <h1 className='heading'>Portafolio</h1>
-      <section className='works'>
-      {
-        works.map((work) => (
-          <article key={work.id} className='work-item'>
-            <div className='mask'>
-              <img src={"/images/" + work.id + ".png"}/>
-            </div>
-            <span>{work.categories}</span>
-            <h2><Link to={"/project/" + work.id}>{work.name}</Link></h2>
-            <h3>{work.technologies}</h3>
-          </article>
-        ))
-      }
-      </section>
+      <Worklist />
     </div>
   )
 }
