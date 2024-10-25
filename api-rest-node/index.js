9a3b7f34d3915ffc59326d965e24bd96c4ec5ff7
AngelIpina12 <angelipina12@hotmail.com>
2024-10-25 00:45:28 -0600

    Primeros pasos con express y cors

--- api-rest-node/index.js
+++ api-rest-node/index.js
@@ -1,5 +1,35 @@
 const connection = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
 
 console.log("Aplicación de NodeJS lista para usar");
 connection();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/probando", (req, res) => {
+  return res.status(200).json([{
+        course: "Master en React",
+        author: "Angel Ipiña",
+        url: "https://master-en-react.com",
+    },
+    {
+        course: "Master en React",
+        author: "Angel Ipiña",
+        url: "https://master-en-react.com",
+    },
+  ]);
+});
+
+app.get("/", (req, res) => {
+    return res.status(200).send(`<h1>Hola mundo con NodeJS y Express</h1>`);
+  });
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en el puerto 3000");
+});
 
