b68427a70f3ac1278162501dd51717ce70e70e1e
AngelIpina12 <angelipina12@hotmail.com>
2024-10-26 11:18:05 -0600

    Creación de directorios y comandos para ejecutar siempre al arrancar proyecto

---
+++ blog-MERN/run-project.bat
@@ -0,0 +1,6 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\8.0\bin && mongod.exe"
+start cmd.exe /k "cd C:\Users\angel\Desktop\Udemy\React\master-react\api-rest-node && npm start"
+start cmd.exe /k "cd C:\Users\angel\Desktop\Udemy\React\master-react\blog-MERN && npm run dev"
+start "" "C:\Users\angel\AppData\Local\MongoDBCompass\MongoDBCompass.exe"
+start "" "C:\Users\angel\AppData\Local\Postman\Postman.exe"
+
