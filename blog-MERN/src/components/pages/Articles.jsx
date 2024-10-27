1da8c886606b0b1da313606c262ac55e951631a5
AngelIpina12 <angelipina12@hotmail.com>
2024-10-27 16:22:17 -0600

    Haciendo petición get con el api rest de blog

--- blog-MERN/src/components/pages/Articles.jsx
+++ blog-MERN/src/components/pages/Articles.jsx
@@ -4,21 +4,30 @@ export const Articles = () => {
   const [articles, setArticles] = useState([])
 
   useEffect(() => {
-    let data = [
-      {
-        _id: 1,
-        title: 'Desarrollo web',
-        content: 'content'
-      }
-    ]
-    setArticles(data)
+    getArticles()
   }, [])
+
+  const getArticles = async () => {
+    const url = 'http://localhost:3000/api/articles'
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    if(response.ok){
+      setArticles(data.articles)
+    }
+  }
 
   return (
     <>
-      {articles.map((article) => (
-        <article key={article._id} className="article-item">
-          <div className='mask'>
+      {
+        articles.length > 0 ?
+        articles.map((article) => (
+          <article key={article._id} className="article-item">
+            <div className='mask'>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/800px-Unofficial_JavaScript_logo_2.svg.png" alt="JavaScript" />
           </div>
           <div className='data'>
@@ -27,8 +36,10 @@ export const Articles = () => {
             <button className="edit">Editar</button>
             <button className="delete">Borrar</button>
           </div>
-        </article>
-      ))}
+          </article>
+        )) :
+        <h2>No hay artículos</h2>
+      }
     </>
   )
 }
