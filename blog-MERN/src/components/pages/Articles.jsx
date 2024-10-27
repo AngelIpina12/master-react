db07cde89d74875ec3752c5da97320c48c77da6d
AngelIpina12 <angelipina12@hotmail.com>
2024-10-27 16:12:03 -0600

    Comenzando a hacer dinámico el componente Articles

--- blog-MERN/src/components/pages/Articles.jsx
+++ blog-MERN/src/components/pages/Articles.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 
 export const Articles = () => {
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    let data = [
+      {
+        _id: 1,
+        title: 'Desarrollo web',
+        content: 'content'
+      }
+    ]
+    setArticles(data)
+  }, [])
+
   return (
     <>
-      <article className="article-item">
+      {articles.map((article) => (
+        <article key={article._id} className="article-item">
           <div className='mask'>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/800px-Unofficial_JavaScript_logo_2.svg.png" alt="JavaScript" />
           </div>
           <div className='data'>
-            <h3 className="title">Desarrollo web</h3>
-            <p className="description">victorroblesweb.es</p>
+            <h3 className="title">{article.title}</h3>
+            <p className="description">{article.content}</p>
             <button className="edit">Editar</button>
             <button className="delete">Borrar</button>
           </div>
-      </article>
+        </article>
+      ))}
     </>
   )
 }
