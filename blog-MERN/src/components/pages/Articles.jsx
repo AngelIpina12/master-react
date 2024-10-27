3a20c41af82ebeddc425e186a07b1d401024aff9
AngelIpina12 <angelipina12@hotmail.com>
2024-10-27 16:04:31 -0600

    Dando estilos al article

--- blog-MERN/src/components/pages/Articles.jsx
+++ blog-MERN/src/components/pages/Articles.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export const Articles = () => {
   return (
-    <div>Articles</div>
+    <>
+      <article className="article-item">
+          <div className='mask'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/800px-Unofficial_JavaScript_logo_2.svg.png" alt="JavaScript" />
+          </div>
+          <div className='data'>
+            <h3 className="title">Desarrollo web</h3>
+            <p className="description">victorroblesweb.es</p>
+            <button className="edit">Editar</button>
+            <button className="delete">Borrar</button>
+          </div>
+      </article>
+    </>
   )
 }
