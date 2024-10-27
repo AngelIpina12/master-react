8afef3b20b786bf6bf20a0716874ba1e34c4abf0
AngelIpina12 <angelipina12@hotmail.com>
2024-10-27 15:42:03 -0600

    Dando estilos al jumbo de bienvenida en home

--- blog-MERN/src/components/pages/Home.jsx
+++ blog-MERN/src/components/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='jumbo'>
+      <h1>Bienvenido a mi Blog</h1>
+      <p>Este es un blog personal donde comparto mis pensamientos y experiencias.</p>
+      <Link to="/articles" className='button'>Ver los artículos</Link>
+    </div>
   )
 }
