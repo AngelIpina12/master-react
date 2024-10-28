7842afd9038dbd4acee6d8f3f8a8da4a04134c52
AngelIpina12 <angelipina12@hotmail.com>
2024-10-28 17:31:06 -0600

    Obtención de datos y guardado del formulario

--- blog-MERN/src/components/layout/Nav.jsx
+++ blog-MERN/src/components/layout/Nav.jsx
@@ -6,7 +6,7 @@ export const Nav = () => {
     <nav className="nav">
       <ul>
         <li><NavLink to="/home">Inicio</NavLink></li>
-        <li><NavLink to="/articles">Películas</NavLink></li>
+        <li><NavLink to="/articles">Artículos</NavLink></li>
         <li><NavLink to="/create">Crear Artículo</NavLink></li>
         <li><NavLink to="/blog">Blog</NavLink></li>
         <li><NavLink to="/contact">Contacto</NavLink></li>
--- blog-MERN/src/components/pages/Create.jsx
+++ blog-MERN/src/components/pages/Create.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
+import { useForm } from '../../hooks/useForm'
+import { ResponseAjax } from '../../helpers/ResponseAjax'
+import { Global } from '../../helpers/Global'
+
 
 export const Create = () => {
+  const {form, sendData, changeData} = useForm({});
+  const [status, setStatus] = useState("no_saved");
+  const saveArticle = async (e) => {
+    e.preventDefault();
+    const {data, loading} = await ResponseAjax(Global.url + 'create', 'POST', form);
+    if(data.status === 'success'){
+      setStatus("saved");
+    }else{
+      setStatus("error");
+    }
+  }
   return (
     <div className='jumbo'>
-      <h3>Crear Artículo</h3>
+      <h1>Crear Artículo</h1>
       <p>Formulario para crear un artículo</p>
-      <form className='form'>
+      {status === "saved" && <strong>Artículo guardado correctamente</strong>}
+      {status === "error" && <strong>Error al guardar el artículo</strong>}
+      <form className='form' onSubmit={saveArticle}>
         <div className='form-group'>
           <label htmlFor='title'>Título</label>
-          <input type='text' name='title' id='title' />
+          <input type='text' name='title' id='title' onChange={changeData} value={form.title} />
         </div>
         <div className='form-group'>
           <label htmlFor='content'>Contenido</label>
-          <textarea name='content' id='content'></textarea>
+          <textarea name='content' id='content' onChange={changeData} value={form.content}></textarea>
         </div>
         <div className='form-group'>
           <label htmlFor='file0'>Imagen</label>
