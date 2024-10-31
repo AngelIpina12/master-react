76107ef0a6446c09ca463584070baf6d065fce53
AngelIpina12 <angelipina12@hotmail.com>
2024-10-30 22:18:33 -0600

    Obtención de imágenes del servidor

--- blog-MERN/src/components/pages/List.jsx
+++ blog-MERN/src/components/pages/List.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Global } from '../../helpers/Global'
 
 export const List = ({articles, setArticles}) => {
   return (
     articles.map((article) => (
         <article key={article._id} className="article-item">
           <div className='mask'>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/800px-Unofficial_JavaScript_logo_2.svg.png" alt="JavaScript" />
+          {article.image != "default.png" ? <img src={`${Global.url}image/${article.image}`} alt={article.title} /> : <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/800px-Unofficial_JavaScript_logo_2.svg.png" alt="JavaScript" />}
         </div>
         <div className='data'>
           <h3 className="title">{article.title}</h3>
