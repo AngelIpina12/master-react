4c1774bfce5d3083b5ace2d797888946a03ad7c5
AngelIpina12 <angelipina12@hotmail.com>
2024-11-04 21:54:51 -0600

    Modificaciones

--- blog-MERN/src/components/pages/Home.jsx
+++ blog-MERN/src/components/pages/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import './Home.css'
+// import './Home.css'
 
 export const Home = () => {
   return (
     <div className='jumbo'>
       <h1>Bienvenido a mi Blog</h1>
-      <p>Este es un blog personal donde comparto mis experiencias y proyectos.</p>
+      <p>Este es un blog personal donde comparto mis experiencias.</p>
       <Link to="/articles" className='button'>Ver los artículos</Link>
     </div>
   )
 }
+
+
