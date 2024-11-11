81a43faa10e5b2362013c79716841b820f4270a5
AngelIpina12 <angelipina12@hotmail.com>
2024-11-10 22:18:56 -0600

    Detalles a cambiar

--- blog-MERN/src/components/pages/Home.jsx
+++ blog-MERN/src/components/pages/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
   return (
     <div className='jumbo'>
       <h1>Bienvenido a mi Blog</h1>
-      <p>Este es un blog personal donde comparto mis experiencias y proyectos.</p>
+      <p>Este es un blog personal donde comparto mis proyectos.</p>
       <Link to="/articles" className='button'>Ver los artículos</Link>
     </div>
   )
