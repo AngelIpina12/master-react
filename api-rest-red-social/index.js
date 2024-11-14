33b319e8dec36b30deebb353b02bd8c7cf41897a
AngelIpina12 <angelipina12@hotmail.com>
2024-11-13 21:24:55 -0600

    Listen de prueba para petición get

--- api-rest-red-social/index.js
+++ api-rest-red-social/index.js
@@ -1,4 +1,25 @@
 const connection = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
 
 console.log("API Node para red social arrancada!")
 connection();
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.get("/ruta-prueba", (req, res) => {
+    return res.status(200).json(
+        {
+            "id": 1,
+            "name": "Angel",
+            "web": "angelipina.com"
+        }
+    )
+});
+app.listen(port, () => {
+    console.log("servidor de node corriendo en el puerto: ", port)
+})
