73880fe78908805862afbd7bbdccc5991a5de4fb
AngelIpina12 <angelipina12@hotmail.com>
2024-12-18 13:38:05 -0600

    Validaciones para controller register

--- api-rest-red-social/controllers/user.js
+++ api-rest-red-social/controllers/user.js
@@ -8,9 +8,35 @@ const testUser = (req, res) => {
 
 const register = (req, res) => {
     let params = req.body;
+    if(!params.name || !params.nick || !params.email || !params.password){
+        return res.status(400).send({
+            status: "error",
+            message: "Faltan datos por enviar"
+        })
+    }
+    let user = new User(params);
+    User.find({$or: [
+        {nick: params.nick},
+        {email: params.email}
+    ]}).exec((err, user) => {
+        if(err){
+            return res.status(500).send({
+                status: "error",
+                message: "Error al registrar el usuario"
+            })
+        }
+        if(user && user.length >= 1){
+            return res.status(200).send({
+                status: "error",
+                message: "El usuario ya existe"
+            })
+        }
+    })
+
     return res.status(200).json({
+        status: "success",
         message: "Acción de registro de usuarios",
-        params
+        user
     })
 }
 
