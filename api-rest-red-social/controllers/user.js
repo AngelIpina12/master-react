7280471e1ed15681896618b353276c7f3654c07a
AngelIpina12 <angelipina12@hotmail.com>
2024-12-18 14:16:08 -0600

    Cifrar contraseñas y guardar usuario

--- api-rest-red-social/controllers/user.js
+++ api-rest-red-social/controllers/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 const testUser = (req, res) => {
@@ -6,39 +7,53 @@ const testUser = (req, res) => {
     })
 }
 
-const register = (req, res) => {
+const register = async (req, res) => {
     let params = req.body;
-    if(!params.name || !params.nick || !params.email || !params.password){
+    if (!params.name || !params.nick || !params.email || !params.password) {
         return res.status(400).send({
             status: "error",
             message: "Faltan datos por enviar"
-        })
+        });
     }
-    let user = new User(params);
-    User.find({$or: [
-        {nick: params.nick},
-        {email: params.email}
-    ]}).exec((err, user) => {
-        if(err){
-            return res.status(500).send({
-                status: "error",
-                message: "Error al registrar el usuario"
-            })
-        }
-        if(user && user.length >= 1){
+
+    try {
+        const user = await User.find({
+            $or: [
+                { nick: params.nick.toLowerCase() },
+                { email: params.email.toLowerCase() }
+            ]
+        });
+
+        if (user && user.length >= 1) {
             return res.status(200).send({
                 status: "error",
                 message: "El usuario ya existe"
-            })
+            });
         }
-    })
 
-    return res.status(200).json({
-        status: "success",
-        message: "Acción de registro de usuarios",
-        user
-    })
-}
+        let pwd = await bcrypt.hash(params.password, 10);
+        params.password = pwd;
+        let user_to_save = new User(params);
+        try {
+            let user_saved = await user_to_save.save();
+            return res.status(200).send({
+                status: "success",
+                message: "Usuario registrado correctamente",
+                user: user_saved
+            });
+        } catch (err) {
+            return res.status(500).send({
+                status: "error",
+                message: "Error al registrar el usuario"
+            });
+        }
+    } catch (err) {
+        return res.status(500).send({
+            status: "error",
+            message: "Error al registrar el usuario"
+        });
+    }
+};
 
 module.exports = {
     testUser,
