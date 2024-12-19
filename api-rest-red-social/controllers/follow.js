09e74b571844adf680b8d7d909d051c42573a71c
AngelIpina12 <angelipina12@hotmail.com>
2024-12-19 15:55:15 -0600

    Preparación de model de follow

--- api-rest-red-social/controllers/follow.js
+++ api-rest-red-social/controllers/follow.js
@@ -1,3 +1,6 @@
+const Follow = require("../models/follow");
+const User = require("../models/user");
+
 const testFollow = (req, res) => {
     return res.status(200).send({
         message: "Mensaje enviado desde: controllers/follow.js"
