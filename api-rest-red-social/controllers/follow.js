1afe4bd97281a487d250246d6e3d81b6d55464f5
AngelIpina12 <angelipina12@hotmail.com>
2024-12-19 16:31:28 -0600

    Método para hacer un follow

--- api-rest-red-social/controllers/follow.js
+++ api-rest-red-social/controllers/follow.js
@@ -8,11 +8,32 @@ const testFollow = (req, res) => {
 }
 
 const save = async (req, res) => {
-    return res.status(200).send({
-        status: "success",
-        message: "Método para seguir a un usuario",
-        identity: req.user
-    })
+    const params = req.body;
+    const identity = req.user;
+    const userToFollow = new Follow({
+        user: identity.id,
+        followed: params.followed
+    });
+    try {
+        let follow = await userToFollow.save();
+        if (!follow) {
+            return res.status(400).send({
+                status: "error",
+                message: "No se ha podido seguir al usuario"
+            });
+        }
+        return res.status(200).send({
+            status: "success",
+            message: "Método para seguir a un usuario",
+            identity,
+            follow
+        })
+    } catch (error) {
+        return res.status(500).send({
+            status: "error",
+            message: "Ha ocurrido un error. Vuelva a intentarlo."
+        });
+    }
 }
 
 module.exports = {
